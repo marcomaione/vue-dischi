55ac091338f65c8d683be9e79f03985663843328
marco maione <maionemarco14@gmail.com>
2022-02-23 16:03:49 +0100

    aggiornamento

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -1,16 +1,29 @@
 <template>
-<div></div>
+<div class="logo">
+    <img src="https://cantabrialabsdifacooper.it/wp-content/uploads/2021/03/png-clipart-spotify-logo-spotify-computer-icons-podcast-music-apps-miscellaneous-angle-1-e1614848134495-300x300.png" alt="">
+</div>
   
 </template>
 
 <script>
 export default {
-    name:myHeader,
+    name:"myHeader",
 
 }
 </script>
 
 <style lang="scss">
+.logo {
+    height: 80px;
+    width: 100%;
+    background-color:#2e3a46;
+    img{
+        max-width: 50px;
+        margin:15px 20px;
+    }
+
+
+}
 
 
 </style>
